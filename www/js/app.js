e6b4de92194914633786aa3fc5376d934517b9b4
Tien Thanh <thanhvt1609@gmail.com>
2016-01-12 13:38:34 +0700

    add remove button

--- www/js/app.js
+++ www/js/app.js
@@ -7,11 +7,21 @@ function onDeviceReady() {
         $("#message").val("");
         e.preventDefault();
     });
+    console.log(navigator.notification);
+    $(document).on('touchend', 'button#btn-delete', function () {
+        var li = $(this).parent().parent();
+        navigator.notification.confirm('Are you sure?', function (buttonIndex) {
+            removeTodo(li);
+        }, 'Confirm', ['Remove', 'Cancle']);
+    });
 }
 function addTodo(message) {
     $("#todo-list").prepend(buildListElement(message));
 }
+function removeTodo(element) {
+    $(element).remove();
+}
 function buildListElement(message) {
-    var li = $("<li></li>").addClass("list-group-item").addClass("clearfix").append(message).append($("<span></span>").addClass("pull-right").append($("<button></button>").addClass("btn btn-xs btn-info").append("Done")).append($("<button></button>").addClass("btn btn-xs btn-warning").append($("<span></span>").addClass("glyphicon glyphicon-trash"))));
+    var li = $("<li></li>").addClass("list-group-item").addClass("clearfix").append(message).append($("<span></span>").addClass("pull-right").append($("<button></button>").addClass("btn btn-xs btn-warning").attr('id', 'btn-delete').append("X")));
     return li;
 }
--- www/js/app.ts
+++ www/js/app.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/jquery/jquery.d.ts" />
+/// <reference path="../../typings/tsd.d.ts" />
 
 $(document).on("deviceready", onDeviceReady);
 
@@ -10,20 +10,33 @@ function onDeviceReady() {
         $("#message").val("");
         e.preventDefault();
     });
+    console.log(navigator.notification);
+    $(document).on('touchend', 'button#btn-delete', function() {
+        var li = $(this).parent().parent();
+        navigator.notification.confirm(
+            'Are you sure?',
+            function(buttonIndex) {
+                removeTodo(li);
+            },
+            'Confirm',
+            ['Remove', 'Cancle']
+            )
+    });
 }
 
-function addTodo(message: string) {
+function addTodo(message: string): void {
     $("#todo-list").prepend(buildListElement(message));
+
+}
+
+function removeTodo(element: any): void {
+    $(element).remove();
 }
 
 function buildListElement(message: string): any {
     var li = $("<li></li>").addClass("list-group-item").addClass("clearfix").append(message).append(
         $("<span></span>").addClass("pull-right").append(
-            $("<button></button>").addClass("btn btn-xs btn-info").append("Done")
-            ).append(
-            $("<button></button>").addClass("btn btn-xs btn-warning").append(
-                $("<span></span>").addClass("glyphicon glyphicon-trash")
-                )
+            $("<button></button>").addClass("btn btn-xs btn-warning").attr('id', 'btn-delete').append("X")
             )
         );
     return li;
