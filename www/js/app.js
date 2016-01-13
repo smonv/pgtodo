0e0db1d86d8aa14e13b340afe2e6acb435db21b2
Tien Thanh <thanhvt1609@gmail.com>
2016-01-13 09:39:13 +0700

    update default files

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,31 @@
 $(document).on("deviceready", onDeviceReady);
+
 function onDeviceReady() {
-    var db = window.sqlitePlugin.openDatabase({ name: "todo.db" });
-    $("#addForm").submit(function (e) {
+    $("#addForm").submit(function(e) {
         var mes;
         mes = $("#message").val();
         addTodo(mes);
         $("#message").val("");
         e.preventDefault();
     });
-    $(document).on('touchend', 'button#btn-delete', function () {
+    $(document).on('touchend', 'button#btn-delete', function() {
         var li = $(this).parent().parent();
-        navigator.notification.confirm('Are you sure?', function (buttonIndex) {
-            removeTodo(li);
-        }, 'Confirm', ['Remove', 'Cancle']);
+        navigator.notification.confirm('Are you sure?', function(buttonIndex) {
+            if(buttonIndex == 1){
+                removeTodo(li);
+            }
+        }, 'Confirm', ['OK', 'Cancle']);
     });
 }
+
 function addTodo(message) {
     $("#todo-list").prepend(buildListElement(message));
 }
+
 function removeTodo(element) {
     $(element).remove();
 }
+
 function buildListElement(message) {
     var li = $("<li></li>").addClass("list-group-item").addClass("clearfix").append(message).append($("<span></span>").addClass("pull-right").append($("<button></button>").addClass("btn btn-xs btn-warning").attr('id', 'btn-delete').append("X")));
     return li;
